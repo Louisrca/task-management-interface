fcb17059af6cbc839acc2aceddc9fcae21883b7f
LouisR17 <roccalouis26@gmail.com>
2025-08-31 16:01:33 +0200

    feature: navbar ok

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -2,14 +2,24 @@ import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { NavLink } from '../../../models/navbar.model';
-import { akarCalendar, akarGrid, akarInbox } from '@ng-icons/akar-icons';
+import {
+  akarCalendar,
+  akarDoor,
+  akarGrid,
+  akarInbox,
+  akarQuestion,
+} from '@ng-icons/akar-icons';
 @Component({
   selector: 'app-navbar',
   imports: [RouterModule, NgIconComponent],
-  providers: [provideIcons({ akarGrid, akarCalendar, akarInbox })],
+  providers: [
+    provideIcons({ akarGrid, akarCalendar, akarInbox, akarDoor, akarQuestion }),
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
   @Input() routes: NavLink[] = [];
+  doorIcon = 'akarDoor';
+  questionIcon = 'akarQuestion';
 }
